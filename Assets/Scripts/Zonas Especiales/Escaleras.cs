54c41edb7ed187903983f6e268adae22471276f3
daniq17 <DanielaArizaLagares@hotmail.com>
2024-07-26 13:05:03 -0400

    Quitar informacion de limites superiores e inferiores a las escaleras

--- Assets/Scripts/Zonas Especiales/Escaleras.cs
+++ Assets/Scripts/Zonas Especiales/Escaleras.cs
@@ -3,13 +3,4 @@ using UnityEngine;
 public class Escaleras : MonoBehaviour
 {
     public int VelocidadSubirEscalera = 2;
-    public float YLimiteSuperior;
-    public float YLimiteInferior;
-
-    private void Start()
-    {
-        YLimiteSuperior = gameObject.GetComponent<Renderer>().bounds.max.y;
-        YLimiteInferior = gameObject.GetComponent<Renderer>().bounds.min.y;
-
-    }
 }
