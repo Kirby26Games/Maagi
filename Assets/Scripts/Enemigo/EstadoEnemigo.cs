252b084bac060c1444a1656570442b23623b56b6
REACTORNUCLEAR\crack <carlosroselloalmerich@gmail.com>
2024-08-01 19:21:49 +0200

    El enemigo puede viajar de forma autónoma a una localización dada. Por defecto, es la última posición donde vio a un objetivo.

--- Assets/Scripts/Enemigo/EstadoEnemigo.cs
+++ Assets/Scripts/Enemigo/EstadoEnemigo.cs
@@ -5,7 +5,8 @@ public class EstadoEnemigo : MonoBehaviour
     public enum Estados { Vigilante, Alerta, Combate }
     public Estados Estado;
     public GameObject ObjetivoFijado;
-    [HideInInspector] public float DistanciaAObstaculo;
+    public Vector3 DestinoFijado;
+    public float DistanciaAObstaculo;
 
     private void Start()
     {
@@ -13,5 +14,26 @@ public class EstadoEnemigo : MonoBehaviour
         Estado = Estados.Vigilante; // null -> Vigilante -> Alerta -> Combate
         ObjetivoFijado = null;
         DistanciaAObstaculo = -1f;
+        DestinoFijado = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        RecordarPosicion();
+    }
+
+    public void RecordarPosicion()
+    {
+        if(ObjetivoFijado != null)
+        {
+            DestinoFijado = ObjetivoFijado.transform.position;
+            return;
+        }
+        else if(Vector3.Distance(transform.position, DestinoFijado) < .5f)
+        {
+            DestinoFijado = Vector3.zero;
+            Estado = Estados.Vigilante;
+            return;
+        }
     }
 }
